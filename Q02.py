0d394ee856986bcf5cd5c383d6bef3e2c086ea27
Reto <2317250093@qq.com>
2022-09-23 23:11:39 +0930

    123

---
+++ Q02.py
@@ -0,0 +1,13 @@
+def saySentence(a, b, c, d, e, f, g):
+    var8 = a + d + b + d + c + e + d + f + d + str(g) + e
+    return var8
+
+var1 = 'Happy'
+var2 = 'New'
+var3 = 'Year'
+var4 = ' '
+var5 = '!'
+var6 = 'It\'s'
+var7 = 2022
+
+print(saySentence(var1, var2, var3, var4, var5, var6, var7))
